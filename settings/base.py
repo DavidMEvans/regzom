6eceeb7edc66b20fd7ac9365672d77453d665859
David Evans <devans6915@aol.com>
2018-02-13 21:33:27 +0000

    Update base.py

--- settings/base.py
+++ settings/base.py
@@ -39,7 +39,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'django.debug_toolbar.middleware.DebugToolbarMiddleware ',
 ]
 
 ROOT_URLCONF = 'RegZom.urls'
